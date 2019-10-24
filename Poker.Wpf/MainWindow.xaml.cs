ac9d0fc50b54c9ff5c42cecf5147f9bdb22cee87
neal <chentianlong621@163.com>
2019-10-23 22:39:07 -0600

    Add WPF application for random test

---
+++ Poker.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using PokerCore;
+using System.Linq;
+
+namespace Poker.Wpf
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var players = new List<PokerPlayer>();
+            for(int i = 0; i < 4; i++)
+            {
+                var cards = new List<PokerCard>();
+                for(int j = 0; j < 5; j++)
+                {
+                    cards.Add(GetRandomCard());
+                }
+                var player = new PokerPlayer { Name = Names[i], PokerCards = cards };
+                players.Add(player);
+            }
+            UpdatePlayers(players);
+            //get winners
+            GetWinners(players);
+        }
+
+        private void GetWinners(List<PokerPlayer> players)
+        {
+            if (_pokerChallenge == null) _pokerChallenge = new PokerChallenge();
+            var winners = _pokerChallenge.GetWinnersOfChallenge(players);
+
+            var nameOfWinners = "";
+            winners.ForEach(x => nameOfWinners += x + " ");
+
+            WinnerText.Text = nameOfWinners;
+        }
+
+        private void UpdatePlayers(List<PokerPlayer> players)
+        {
+            var count = 0;
+            foreach(var child in PlayersPanel.Children)
+            {
+                if(child is PlayerItem item)
+                {
+                    item.PlayerName = players[count].Name;
+                    item.SetValuesByCards(players[count].PokerCards.ToList());
+                    count++;
+                }
+            }
+        }
+
+        private PokerCard GetRandomCard()
+        {
+            if(_random==null) _random = new Random(DateTime.Now.Millisecond);
+            var rank = _random.Next(2, 15);
+            var suit = _random.Next(0, 4);
+            return new PokerCard { Rank = rank, Suit = (PokerSuits)suit };
+        }
+
+        private Random _random;
+
+        private string[] Names = new[] { "Joe", "Bob", "Jen", "Kai" };
+
+        private IPokerChallenge _pokerChallenge;
+    }
+}
---
+++ Poker.Wpf/PlayerItem.xaml.cs
@@ -0,0 +1,97 @@
+﻿using PokerCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Poker.Wpf
+{
+    /// <summary>
+    /// PlayerItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class PlayerItem : UserControl, INotifyPropertyChanged
+    {
+        public PlayerItem()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string PlayerName
+        {
+            get => _playerName;
+            set
+            {
+                _playerName = value;
+                OnPropertyChanged(nameof(PlayerName));
+            }
+        }
+        private string _playerName = "Joe";
+
+        public List<PlayerItemModel> Values
+        {
+            get => _values;
+            set
+            {
+                _values = value;
+                OnPropertyChanged(nameof(Values));
+            }
+        }
+
+        private List<PlayerItemModel> _values = new List<PlayerItemModel>
+        {
+            new PlayerItemModel{Rank="2",Image="./Resource/spade.png"},
+            new PlayerItemModel{Rank="A",Image="./Resource/spade.png"},
+            new PlayerItemModel{Rank="K",Image="./Resource/spade.png"},
+            new PlayerItemModel{Rank="4",Image="./Resource/spade.png"},
+            new PlayerItemModel{Rank="5",Image="./Resource/spade.png"},
+        };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetValuesByCards(List<PokerCard> cards)
+        {
+            var values = new List<PlayerItemModel>();
+            foreach(var card in cards)
+            {
+                var model = new PlayerItemModel { Rank = GetCorrectRank(card.Rank) };
+                model.Image = $"./Resource/{card.Suit.ToString().ToLower()}.png";
+                values.Add(model);
+            }
+            Values = values;
+        }
+
+        private string GetCorrectRank(int rank)
+        {
+            if (rank == 11) return "J";
+            if (rank == 12) return "Q";
+            if (rank == 13) return "K";
+            if (rank == 14) return "A";
+            return rank.ToString();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged == null) return;
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class PlayerItemModel
+    {
+        public string Rank { get; set; }
+
+        public string Image { get; set; }
+    }
+}
